67f3f15b4553763e244ba0516464b1dc707c732a
Yichen Fan <peteryc.fan@mail.utoronto.ca>
2024-12-01 19:24:45 -0500

    upload test analysis

---
+++ scripts/04-test_analysis_data.R
@@ -0,0 +1,64 @@
+#### Preamble ####
+# Purpose: Tests the analysis data for integrity and structure
+# Author: Peter Fan
+# Date: 21 November 2024
+# Contact: peteryc.fan@mail.utoronto.ca
+# Pre-requisites:
+# - Ensure the 'tidyverse', 'arrow', testthat' packages are installed 
+
+#### Workspace setup ####
+if (!requireNamespace("tidyverse", quietly = TRUE)) {
+  install.packages("tidyverse")
+}
+if (!requireNamespace("testthat", quietly = TRUE)) {
+  install.packages("testthat")
+}
+if (!requireNamespace("arrow", quietly = TRUE)) {
+  install.packages("arrow", repos = c("https://apache.r-universe.dev", "https://cloud.r-project.org"))
+}
+if (!requireNamespace("here", quietly = TRUE)) {
+  install.packages("here")
+}
+
+library(tidyverse)
+library(testthat)
+library(arrow)
+library(here)
+
+
+
+# Load the cleaned data
+analysis_data <- read_parquet(here("data/02-analysis_data/analysis_data.parquet"))
+
+#### Test data ####
+# Test that the dataset has unique ISBN in each record or row no duplicate
+test_that("the dataset has unique ISBNs", {
+  expect_equal(length(unique(analysis_data$isbn)), nrow(analysis_data))
+})
+
+# Test that there are 8 columns in the dataset
+test_that("'8 columns in dataset", {
+  expect_equal(ncol(analysis_data), 8)
+})
+
+# Test that there are less than 10 types of book covers in the dataset 
+test_that("dataset has 10 types of book covers", {
+  expect_lt(length(unique(analysis_data$cover)), 10)
+})
+
+# Test that the 'cover' column is character type
+test_that("'cover' is character", {
+  expect_type(analysis_data$cover, "character")
+})
+
+
+# Test that all remaining columns except isbn and cover are numaric
+test_that("all columns except 'isbn' and 'cover' are numeric", {
+  columns_to_check <- analysis_data[,3:8]
+  expect_true(all(sapply(columns_to_check, is.numeric)))
+})
+
+# Test that there are no missing values in the dataset
+test_that("no missing values in dataset", {
+  expect_true(all(!is.na(analysis_data)))
+})
